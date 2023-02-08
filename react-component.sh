e1c14f4d3329ed58bdd602565cd9781aaa1a8923
Ned Watson <watsonle@iastate.edu>
2023-02-08 18:49:35 -0500

    fix other bug in scaffolding

--- react-component.sh
+++ react-component.sh
@@ -22,7 +22,7 @@ function scaffoldReactComponent () {
   echo "import styles from './$1.module.css'
 const $1 = () => {} 
 export default $1" >> $2
-  echo "export * from './$2.js'" >> index.js
+  echo "export * from './$2'" >> index.js
 }
 
 for component in ${toCreate[@]}; do
